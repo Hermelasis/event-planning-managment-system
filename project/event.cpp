5485e12c6b2de4511adc74c03da607633f5a12eb
bookquotesgirl <157220349+bookquotesgirl@users.noreply.github.com>
2024-05-26 20:53:39 +0300

    Update event.cpp

--- project/event.cpp
+++ project/event.cpp
@@ -126,3 +126,132 @@ void connect_db()
         exit(1);
     }
 }
+// Function to close the database connection
+void close_db()
+{
+    if (res != NULL)
+    {
+        mysql_free_result(res); // Free the result set
+    }
+    mysql_close(conn); // Close the MySQL connection
+}
+
+// Function to register a new user
+void register_user()
+{
+    string username, password;
+    cout << "Enter username: ";
+    cin >> username;
+    cout << "Enter password: ";
+    cin >> password;
+
+    string query = "INSERT INTO users (username, password) VALUES ('" + username + "', '" + password + "')"; // Inserting to database
+    if (mysql_query(conn, query.c_str()))
+    {
+        cout << "Registration failed: " << mysql_error(conn) << endl;
+    }
+    // Automatically Logs in after the user registed
+    else
+    {
+        cout << "Registration successful! Logged in successfully" << endl;
+        user_dashboard(username);
+    }
+}
+
+// Function to authenticate a user
+bool authenticate_user(string username, string password)
+{
+    string query = "SELECT * FROM users WHERE username = '" + username + "' AND password = '" + password + "'"; // Finding the user with the same name and password
+    if (mysql_query(conn, query.c_str()))
+    {
+        cout << "Authentication failed: " << mysql_error(conn) << endl;
+        return false;
+    }
+
+    res = mysql_store_result(conn); // Store the result set
+    if (res == NULL)
+    {
+        cout << "Error check the server: " << mysql_error(conn) << endl;
+        return false;
+    }
+
+    row = mysql_fetch_row(res); // Fetch the row
+    if (row == NULL)
+    {
+        cout << "Invalid username or password" << endl;
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+}
+
+// Function to post a new event (admin only)
+void post_event()
+{
+    string name, date;
+    double price;
+    int max_capacity;
+
+    cout << "Enter event name: ";
+    cin.ignore();
+    getline(cin, name);
+
+    cout << "Enter event date (YYYY-MM-DD): ";
+    getline(cin, date);
+
+    cout << "Enter event price: ";
+    cin >> price;
+
+    cout << "Enter event max capacity: ";
+    cin >> max_capacity;
+
+    string query = "INSERT INTO events (name, date, price, max_capacity) VALUES ('" + name + "', '" + date + "', " + string_to_int(price) + ", " + string_to_int(max_capacity) + ")"; // INserting the Event to events dataabase
+    if (mysql_query(conn, query.c_str()))
+    {
+        cout << "Failed to post event: " << mysql_error(conn) << endl;
+    }
+    else
+    {
+        cout << "Event posted successfully!" << endl;
+    }
+}
+
+// Function to view all events
+void view_events(string username)
+{
+    string query = "SELECT * FROM events"; // Printing all the rows in events table
+    if (mysql_query(conn, query.c_str()))
+    {
+        cout << "Failed to retrieve events: " << mysql_error(conn) << endl;
+        return;
+    }
+
+    res = mysql_store_result(conn); // Store the result set
+    if (res == NULL)
+    {
+        cout << "Failed to retrieve events: " << mysql_error(conn) << endl;
+        return;
+    }
+
+    cout << "Id" << "\t" << "Name" << "\t" << "Date" << "\t\t" << "Price" << "\t" << "Max" << "\t" << "current_capacity" << endl;
+    int num_fields = mysql_num_fields(res);
+    while ((row = mysql_fetch_row(res)))
+    {
+        for (int i = 0; i < num_fields; i++)
+        {
+            cout << row[i] << "\t";
+        }
+        cout << endl;
+    }
+
+    int choice;
+    cout << "Enter the event ID to book or 0 to go back: ";
+    cin >> choice;
+
+    if (choice != 0)
+    {
+        book_event(username, choice);
+    }
+}
